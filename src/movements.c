8223ff64a895e678fa311180eae4b4c98a2d3912
Antonin Chantier <antonin.chantier@epita.fr>
2018-11-10 21:49:55 +0100

    [PLAYER] Fixed movements

--- src/movements.c
+++ src/movements.c
@@ -2,15 +2,6 @@
 
 #define BLOCK_SIZE 20
 
-/*
-struct vector2 *jump(struct Character *character)
-{
-    struct vector2 position;
-    struct vector2 velocity;
-    struct vector2 f
-}
-*/
-
 // Jump
 //
 // f(t) = 1/2 * gt^2 + vt + p   g gravity
@@ -20,56 +11,56 @@ struct vector2 *jump(struct Character *character)
 // v = 2h / (th)   th time at which peak height is reached
 //
 // g = -2h / (th)^2   h peak height
-//
-// th = xh / (vx)
-// v = 2h(vx) / (xh)
-// g = -2h(vx)^2 / (xh)^2
 
 float apply_gravity(struct Character *character, struct GameState *gs)
 {
+    float x = character->position.x;
+    float y = character->position.y;
     float delta_time = 1 / 60;
     float g = 9.8 + character->y_acc;
     float speed = g * delta_time;
-    float fall = character->y + speed * delta_time;
-    character->y_acc += character->y >= 0 ? -character->y : g * delta_time;
+    float fall = speed * delta_time;
+    character->y_acc += y >= 0 ? -y : g * delta_time;
 
-    if (gs->map[character->x * 30 + MAP_WIDT * fall * 30] == AIR)
+    if (gs->map[x * 30 + WINDOW_WIDTH * fall * 30] == AIR)
         return fall;
 
     int i = 1;
 
-    while (gs->map[character->x * 30 + MAP_WIDT * (character->y - i) * 30] == AIR)
+    while (gs->map[x * 30 + WINDOW_WIDTH * (y - i) * 30] == AIR)
         i++;
 
     character->status = IDLE;
-    return character->y - i;
+    return y - i;
 }
 
 void update_position(struct Character *character, enum action action,
                      struct GameState *gs)
 {
-    int index = character->x * 30 + Map_WIDTH * (character->y + 1) * 30;
+    float x = character->position.x;
+    float y = character->position.y;
+    int index = x * 30 + WINDOW_WIDTH * (y + 1) * 30;
 
-    if (action & JUMP && map[index] != AIR)
+    if (action & JUMP && gs->map[index] != AIR)
     {
-        character->y_acc = -12f;
+        character->y_acc = -12.0;
         character->status |= JUMPING;
     }
 
-    if (character->status & JUMP || map[index] == AIR)
-        character->y += apply_gravity(character, gs);
+    if (character->status & JUMP || gs->map[index] == AIR)
+        character->position.y += apply_gravity(character, gs);
 
-    index = (character->x - 1) * 30 + MAP_WIDTH * character->y * 30;
+    index = (x - 1) * 30 + WINDOW_WIDTH * y * 30;
     if ((action & LEFT) && gs->map->blocks[index].state == AIR)
     {
-        character->x -= 1;
+        character->position.x -= 1;
         character->status |= RUNNING;
     }
 
-    index = (character->x + 1) * 30 + MAP_WIDTH * character->y * 30;
+    index = (x + 1) * 30 + WINDOW_WIDTH * y * 30;
     if ((action & RIGHT) && gs->map->blocks[index].state == AIR)
     {
-        character->x += 1;
+        character->position.x += 1;
         character->status |= RUNNING;
     }
 
