626510b6c40358687b3583917ba29e6685ea75d9
Céline WU <celine.wu@epita.fr>
2018-11-10 13:47:29 +0100

    [MVT] Add movements.c with instructions for jumping

---
+++ src/movements.c
@@ -0,0 +1,15 @@
+#include "character.h"
+
+// Jump
+//
+// f(t) = 1/2 * gt^2 + vt + p   g gravity
+//                              v initial velocity
+//                              p initital position
+//
+// v = 2h / (th)   th time at which peak height is reached
+//
+// g = -2h / (th)^2   h peak height
+//
+// th = xh / (vx)
+// v = 2h(vx) / (xh)
+// g = -2h(vx)^2 / (xh)^2
