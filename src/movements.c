c3d04a8a92a207262768dbb79d3acf50d11a8f9b
Céline WU <celine.wu@epita.fr>
2018-11-10 16:25:47 +0100

    [CHAR] movements.c: corrected compile errors

--- src/movements.c
+++ src/movements.c
@@ -2,6 +2,15 @@
 
 #define BLOCK_SIZE 20
 
+/*
+struct vector2 *jump(struct Character *character)
+{
+    struct vector2 position;
+    struct vector2 velocity;
+    struct vector2 f
+}
+*/
+
 // Jump
 //
 // f(t) = 1/2 * gt^2 + vt + p   g gravity
