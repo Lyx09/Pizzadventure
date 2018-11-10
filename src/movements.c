1c1c361307e8b8937186c5d31d0371d0aa5a3ffb
Antonin Chantier <antonin.chantier@epita.fr>
2018-11-10 20:58:05 +0100

    [PLAYER] movements.c: Managing player mouvement, added function for gravity

--- src/movements.c
+++ src/movements.c
@@ -15,3 +15,55 @@
 // th = xh / (vx)
 // v = 2h(vx) / (xh)
 // g = -2h(vx)^2 / (xh)^2
+
+float apply_gravity(struct Character *character, struct GameState *gs)
+{
+    float delta_time = 1 / 60;
+    float g = 9.8 + character->y_acc;
+    float speed = g * delta_time;
+    float fall = character->y + speed * delta_time;
+    character->y_acc += character->y >= 0 ? -character->y : g * delta_time;
+
+    if (gs->map[character->x * 30 + MAP_WIDT * fall * 30] == AIR)
+        return fall;
+
+    int i = 1;
+
+    while (gs->map[character->x * 30 + MAP_WIDT * (character->y - i) * 30] == AIR)
+        i++;
+
+    character->status = IDLE;
+    return character->y - i;
+}
+
+void update_position(struct Character *character, enum action action,
+                     struct GameState *gs)
+{
+    int index = character->x * 30 + Map_WIDTH * (character->y + 1) * 30;
+
+    if (action & JUMP && map[index] != AIR)
+    {
+        character->y_acc = -12f;
+        character->status |= JUMPING;
+    }
+
+    if (character->status & JUMP || map[index] == AIR)
+        character->y += apply_gravity(character, gs);
+
+    index = (character->x - 1) * 30 + MAP_WIDTH * character->y * 30;
+    if ((action & LEFT) && gs->map->blocks[index].state == AIR)
+    {
+        character->x -= 1;
+        character->status |= RUNNING;
+    }
+
+    index = (character->x + 1) * 30 + MAP_WIDTH * character->y * 30;
+    if ((action & RIGHT) && gs->map->blocks[index].state == AIR)
+    {
+        character->x += 1;
+        character->status |= RUNNING;
+    }
+
+    if (!action)
+        character->status &= ~RUNNING;
+}
