f9fb8dcd1b5eb4dd6b5f5806385db1d8bfafd60c
William Lin <william.lin@epita.fr>
2018-11-11 00:35:21 +0100

    [RENDER] Add render files

---
+++ include/render.h
@@ -0,0 +1,8 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include "game.h"
+
+void render_frame(struct GameState *gs);
+
+#endif
---
+++ src/render.c
@@ -0,0 +1,7 @@
+#include "game.h"
+#include "render.h"
+
+void render_frame(struct GameState *gs)
+{
+
+}
