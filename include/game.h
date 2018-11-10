b05a5145c6e25664f541f294269c9970425e3a64
William Lin <william.lin@epita.fr>
2018-11-10 13:51:17 +0100

    [GAME] game.h: Add macros

--- include/game.h
+++ include/game.h
@@ -1,7 +1,14 @@
 #ifndef GAME_H
 # define GAME_H
 
+#include <SDL.h>
 
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define MUSIC_GAME_LAUNCH "./resources/music/opening.mp3"
+#define MUSIC_INTRO ""
+#define IMAGE_TITLE "./resources/background/cyberpunk-street.png"
 
 struct GameState
 {
