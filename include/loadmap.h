68f18868c58cf24e22d78cb8178aae50b072033c
William Lin <william.lin@epita.fr>
2018-11-11 07:27:30 +0100

    [LOADMAP] Start loadmap.c

---
+++ include/loadmap.h
@@ -0,0 +1,9 @@
+#ifndef LOADMAP_H
+#define LOADMAP_H
+
+#include "game.h"
+#include "map.h"
+
+void load_map(struct GameState *gs);
+
+#endif /* ! LOADMAP_H */
---
+++ src/loadmap.c
@@ -0,0 +1,9 @@
+#include "loadmap.h"
+#include "map.h"
+#include "game.h"
+
+void load_map(struct GameState *gs)
+{
+    SDL_RenderClear(gs->renderer);
+    (void) gs;
+}
