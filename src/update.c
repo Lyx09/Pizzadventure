3dd5dbfb08aa18470ee58d1ec3745e6efd216699
William Lin <william.lin@epita.fr>
2018-11-11 07:00:03 +0100

    [UPDATE] game.c: Add delta_time function

--- src/update.c
+++ src/update.c
@@ -3,7 +3,8 @@
 #include "input.h"
 #include "character.h"
 
-void update(struct GameState *gs, enum action action)
+void update(struct GameState *gs, enum action action, double dt_time)
 {
+    (void) dt_time;
     update_position(gs->player, action, gs);
 }
